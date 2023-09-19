bd6e9997efd19c6929ae9d64b7f334bf8ea5ca5b
eric <ericopokuantwi384@gmail.com>
2023-09-19 08:35:08 +0000

    solve deployment issue

--- db/migrate/20230910224323_remove_column_from_rentals.rb
+++
@@ -1,7 +0,0 @@
-class RemoveColumnFromRentals < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :rentals, :cars_id, :bigint
-    remove_column :rentals, :users_id, :bigint
-    remove_column :payments, :rentals_id, :bigint
-  end
-end
