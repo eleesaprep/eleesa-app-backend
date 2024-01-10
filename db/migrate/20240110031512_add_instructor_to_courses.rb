7e099aa9d52eb2c98ee5452dbb4c0d4c2fe83306
eric <ericopokuantwi384@gmail.com>
2024-01-10 03:18:39 +0000

    finish backend

--- db/migrate/20240110031512_add_instructor_to_courses.rb
+++ db/migrate/20240110031512_add_instructor_to_courses.rb
@@ -6,7 +6,11 @@ class AddInstructorToCourses < ActiveRecord::Migration[7.0]
     add_reference :materials, :course, null: false, foreign_key: true
     add_reference :progresses, :user, null: false, foreign_key: true
     add_reference :progresses, :course, null: false, foreign_key: true
+    add_reference :enrollments, :user, null: false, foreign_key: true
     add_reference :enrollments, :course, null: false, foreign_key: true
     add_reference :progresses, :quiz, null: false, foreign_key: true
+    add_index :enrollments, [:user_id, :course_id], unique: true
+    add_index :options, [:option_text, :question_id], unique: true
+    add_index :progresses, [:user_id, :quiz_id], unique: true
   end
 end
